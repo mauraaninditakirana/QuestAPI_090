eca894cd14f83438427bfcbeb1fe6e186b3bd8fe
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-18 12:51:08 +0700

    menambahkan EntryViewModel untuk input dan validasi data siswa

---
+++ app/src/main/java/com/example/praktikumlocalrestapi/viewmodel/EntryViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.praktikumlocalrestapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.praktikumlocalrestapi.modeldata.DetailSiswa
+import com.example.praktikumlocalrestapi.modeldata.UIStateSiswa
+import com.example.praktikumlocalrestapi.modeldata.toDataSiswa
+import com.example.praktikumlocalrestapi.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+        ViewModel(){
+            var uiStateSiswa by mutableStateOf(UIStateSiswa())
+            private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
+            Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid=
+                validasiInput(detailSiswa))
+    }
+    suspend fun addSiswa(){
+        if (validasiInput()) {
+            val sip: Response<Void> =repositoryDataSiswa
+                .postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if(sip.isSuccessful){
+                println("Sukses Tambah Data: ${sip.message()}")
+            }else{
+                printIn("Gagal Tambah Data: ${sip.errorBody()}")
+            }
+        }
+    }
+        }
