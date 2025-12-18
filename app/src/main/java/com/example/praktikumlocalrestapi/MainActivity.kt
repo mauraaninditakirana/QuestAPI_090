885c5de1223fc41607e0c2618e76eb4ee18b4f1a
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-18 13:48:06 +0700

    mengubah greeting menjadi nama app di MainActivity.kt

--- app/src/main/java/com/example/praktikumlocalrestapi/MainActivity.kt
+++ app/src/main/java/com/example/praktikumlocalrestapi/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikumlocalrestapi.ui.theme.PraktikumLocalRestAPITheme
+import com.example.praktikumlocalrestapi.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PraktikumLocalRestAPITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
