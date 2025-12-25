dad5a9e6ce922e860186652bd9a2fc96666760b6
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 20:31:03 +0700

    membuat DestinasiDetail.kt untuk route halaman detail

---
+++ app/src/main/java/com/example/praktikumlocalrestapi/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.praktikumlocalrestapi.uicontroller.route
+
+import com.example.praktikumlocalrestapi.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
